661872bb4b42d705b45a9c34b87b6907808e8825
Roman-Lviv <kituraroman@gmail.com>
2023-09-24 13:59:26 +0300

    add

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,12 @@
-// Отримуємо елемент ul#categories
-const categoriesList = document.querySelector('#categories');
+const categoriesList = document.querySelector("#categories");
 
-// Отримуємо всі елементи li.item у списку ul#categories
-const categoryItems = categoriesList.querySelectorAll('li.item');
+const categoryItems = categoriesList.querySelectorAll("li.item");
 
-// Виводимо кількість категорій
 console.log(`Number of categories: ${categoryItems.length}`);
 
-// Ітеруємось по кожному елементу li.item
 categoryItems.forEach((categoryItem) => {
-  // Отримуємо заголовок категорії (тег <h2>)
-  const categoryTitle = categoryItem.querySelector('h2');
-  
-  // Отримуємо всі елементи <li> у даній категорії
-  const categoryElements = categoryItem.querySelectorAll('ul li');
-  
-  // Виводимо інформацію про категорію
+  const categoryTitle = categoryItem.querySelector("h2");
+  const categoryElements = categoryItem.querySelectorAll("ul li");
   console.log(`Category: ${categoryTitle.textContent}`);
   console.log(`Elements: ${categoryElements.length}`);
 });
--- js/task-10.js
+++ js/task-10.js
@@ -1,13 +1,6 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
-}
-
-// Функція для генерації випадкового кольору
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
     .padStart(6, "0")}`;
 }
 
